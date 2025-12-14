ab67550e30a1b8bc89e0bafb703f46c3dfcbdb38
Galindo97 <rexogamer17@gmail.com>
2025-12-14 15:25:04 -0600

    verifi

--- project-manager/frontend/svelte.config.js
+++ project-manager/frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,8 +6,13 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { 
-		adapter: adapter(),
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: '/project-manager'
+		},
 		alias: {
 			$stores: 'src/stores'
 		}
